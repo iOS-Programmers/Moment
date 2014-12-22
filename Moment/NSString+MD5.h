a464c4701a6d68c8fdcdf428ea77ac604c7214d2
Johnny <iyhjiang@gmail.com>
2014-12-22 22:36:06 +0800

    xx

---
+++ Moment/NSString+MD5.h
@@ -0,0 +1,14 @@
+//
+//  NSString+MD5.h
+//  Decoworld
+//
+//  Created by ouran on 13-8-11.
+//  Copyright (c) 2013年 Iceland. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (MD5)
+- (NSString *)md5;
+
+@end
