5ba2acb57cb10be0c7c321ef96f3ce54c361f162
Johnny <iyhjiang@gmail.com>
2015-01-29 17:09:16 +0800

    添加长图拼接模式

--- Moment/Controllers/CatchMomentViewController.m
+++ Moment/Controllers/CatchMomentViewController.m
@@ -10,15 +10,14 @@
 #import "EditStoryViewController.h"
 
 #import "UploadPictureHttp.h"
-#import "AdMomentHttp.h"
 
 @interface CatchMomentViewController () <UIActionSheetDelegate,UIImagePickerControllerDelegate,UINavigationControllerDelegate>
 
-@property (strong, nonatomic) UploadPictureHttp *updatePicHttp;
-@property (strong, nonatomic) AdMomentHttp *addMomentHttp;
+@property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
 
-@property (weak, nonatomic) IBOutlet UIImageView *imageView;
-@property (strong, nonatomic) NSMutableArray *imageIds;   //存图片ID的数组
+@property (strong, nonatomic) UploadPictureHttp *updatePicHttp;
+
+@property (strong, nonatomic) NSMutableArray *images;   //存图片的数组
 
 
 - (IBAction)onNextBtnClick:(UIButton *)sender;
@@ -37,13 +36,16 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
     self.title = @"抓住瞬间";
-    self.imageIds = [[NSMutableArray alloc] initWithCapacity:1];
+    self.images = [[NSMutableArray alloc] initWithCapacity:1];
     
     self.updatePicHttp = [[UploadPictureHttp alloc] init];
-    self.addMomentHttp = [[AdMomentHttp alloc] init];
     
+    [self.scrollView setContentSize:CGSizeMake([LXUtils GetScreeWidth] * 2, [LXUtils getContentViewHeight] - 120)];
+    self.scrollView.pagingEnabled = YES;
+    self.scrollView.showsVerticalScrollIndicator = NO;
+    self.scrollView.backgroundColor = [UIColor colorWithRed:1 green:1 blue:1 alpha:0.5];
     //测试上传拼接的长图
-    [self uploadImage:nil];
+//    [self uploadImage:nil];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -61,45 +63,36 @@
 }
 */
 
-#pragma mark - RequestMethod
-
-- (void)addMomentRequest
+- (void)updateUI
 {
-    self.addMomentHttp.parameter.fid = @"2";
-    self.addMomentHttp.parameter.title = @"重大消息！阿里巴巴收购腾讯！！！！";
-    self.addMomentHttp.parameter.content = @"阿里巴巴在昨晚宣布，收购腾讯百分之八十的股权，这在历史上是前无古人后无来者的，同时马化腾成为马云的手下。";
-
-    [self showLoadingWithText:MT_LOADING];
-    __block CatchMomentViewController *weak_self = self;
-    [self.addMomentHttp getDataWithCompletionBlock:^{
-        [weak_self hideLoading];
-        if (weak_self.addMomentHttp.isValid)
-        {
-            [weak_self showWithText:@"发布成功"];
-        }
-    }failedBlock:^{
-        [weak_self hideLoading];
-        if (![LXUtils networkDetect])
-        {
-            [weak_self showWithText:MT_CHECKNET];
-        }
-        else
-        {   //统统归纳为服务器出错
-            [weak_self showWithText:MT_NETWRONG];
-        };
-    }];
-
+    for (int i = 0; i < [self.images count]; i ++) {
+        UIImageView * imgView = [[UIImageView alloc] init];
+        imgView.frame = CGRectMake([LXUtils GetScreeWidth] * i,0 , [LXUtils GetScreeWidth], self.scrollView.frame.size.height);
+        imgView.image = self.images[i];
+        
+        [self.scrollView addSubview:imgView];
+        
+    }
+    
+    self.scrollView.contentSize = CGSizeMake([LXUtils GetScreeWidth] *([self.images count] +1), self.scrollView.frame.size.height);
 }
+
 
 #pragma mark - IBAciton
 - (IBAction)onNextBtnClick:(UIButton *)sender {
 
-    EditStoryViewController *editVC = [[EditStoryViewController alloc] init];
-    editVC.hidesBottomBarWhenPushed = YES;
+    /**
+       先拼接长图，然后再上传，上传成功后，再跳转
+     */
+    if ([self.images count] < 1) {
+        [self showWithText:@"请至少上传一张图片!"];
+        return;
+    }
     
-    editVC.imageIds = self.imageIds;
+    UIImage *uploadImage  = [self addImageWithImageArray:self.images];
     
-    [self.navigationController pushViewController:editVC animated:YES];
+    [self uploadImage: uploadImage];
+    
 }
 
 
@@ -153,7 +146,7 @@
 
     UITextField *firstTF = [[UITextField alloc] init];
     firstTF.frame = CGRectMake(0, 0, 200, 40);
-    firstTF.center = self.imageView.center;
+    firstTF.center = self.scrollView.center;
     firstTF.textColor = [UIColor whiteColor];
     firstTF.text = @"此处文字可随意拖动!";
 
@@ -167,21 +160,43 @@
 {
    
     UIImage *rightImage = [LXUtils rotateImage:image];
+    
+    [self.images addObject:rightImage];
+    
+    [self updateUI];
 
     [picker dismissViewControllerAnimated:YES completion:nil];
    
 }
 
-- (UIImage *)addImage:(UIImageView *)image1 toImage:(UIImageView *)image2 {
-    CGSize size= CGSizeMake(image1.frame.size.width,image1.frame.size.height+image2.frame.size.height);
+/**
+ *  把数组里的image拼接成长图
+ *
+ *  @param arr 装image的数组
+ *
+ *  @return 返回单张uiimage
+ */
+- (UIImage *)addImageWithImageArray:(NSMutableArray *)arr
+{
+    if ([arr count] < 1) {
+        return nil;
+    }
+    
+    UIImage *image1 = (UIImage *)arr[0];
+    CGSize size= CGSizeMake([LXUtils GetScreeWidth],image1.size.height * [arr count]);
     UIGraphicsBeginImageContext(size);
     
+    for (int i = 0; i< [arr count]; i ++) {
+        UIImage *image = [[UIImage alloc] init];
+        image = (UIImage *)arr[i];
+        [image drawInRect:CGRectMake(0, image1.size.height * i, image1.size.width, image1.size.height)];
+    }
     // Draw image1
-    [image2.image drawInRect:CGRectMake(0, 0, image1.frame.size.width, image1.frame.size.height)];
-    
-    // Draw image2
-    [image1.image drawInRect:CGRectMake(0, image1.frame.size.height, image2.frame.size.width, image2.frame.size.height)];
-    
+//    [image2.image drawInRect:CGRectMake(0, 0, image1.frame.size.width, image1.frame.size.height)];
+//    
+//    // Draw image2
+//    [image1.image drawInRect:CGRectMake(0, image1.frame.size.height, image2.frame.size.width, image2.frame.size.height)];
+//    
     UIImage *resultingImage = UIGraphicsGetImageFromCurrentImageContext();
     
     UIGraphicsEndImageContext();
@@ -189,13 +204,25 @@
     return resultingImage;
 }
 
+//- (UIImage *)addImage:(UIImageView *)image1 toImage:(UIImageView *)image2 {
+//    CGSize size= CGSizeMake(image1.frame.size.width,image1.frame.size.height+image2.frame.size.height);
+//    UIGraphicsBeginImageContext(size);
+//    
+//    // Draw image1
+//    [image2.image drawInRect:CGRectMake(0, 0, image1.frame.size.width, image1.frame.size.height)];
+//    
+//    // Draw image2
+//    [image1.image drawInRect:CGRectMake(0, image1.frame.size.height, image2.frame.size.width, image2.frame.size.height)];
+//    
+//    UIImage *resultingImage = UIGraphicsGetImageFromCurrentImageContext();
+//    
+//    UIGraphicsEndImageContext();
+//    
+//    return resultingImage;
+//}
+
 - (void)uploadImage:(UIImage *)rightImage
 {
-    UIImageView *imageView1 = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"1-640x960"]];
-    UIImageView *imageView2 = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"2-640x960"]];
-    
-    rightImage = [self addImage:imageView1 toImage:imageView2];
-    
     self.updatePicHttp.parameter.image = rightImage;
     [self showLoadingWithText:MT_LOADING];
     __block CatchMomentViewController *weak_self = self;
@@ -206,15 +233,13 @@
             
             if (weak_self.updatePicHttp.resultModel.avatar) {
                 
-                if (![weak_self.imageIds containsObject:weak_self.updatePicHttp.resultModel.avatar]) {
-                    [weak_self.imageIds addObject:weak_self.updatePicHttp.resultModel.avatar];
-                }
+                EditStoryViewController *editVC = [[EditStoryViewController alloc] init];
+                editVC.hidesBottomBarWhenPushed = YES;
                 
-                if ([weak_self.imageIds count] > 0) {
-                    weak_self.addMomentHttp.parameter.pictureurls = [weak_self.imageIds componentsJoinedByString:@","];
-                }
+                editVC.imageIds = [NSMutableArray arrayWithArray:@[self.updatePicHttp.resultModel.avatar]];
                 
-                [self.imageView sd_setImageWithURL:[NSURL URLWithString:[NSString stringWithFormat:@"%@%@",IMAGE_PRE,weak_self.updatePicHttp.resultModel.avatar]]];
+                [weak_self.navigationController pushViewController:editVC animated:YES];
+
             }
             
         }
